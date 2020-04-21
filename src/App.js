7af214d80c5a507f893b2f0145400ca0b20a81d4
Denis Zinchenko <zinchenko.denis.work@gmail.com>
2020-04-21 22:41:59 +0300

    add back & show fuul info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect }  from 'react';
 import axios from 'axios'
+import Button from "@material-ui/core/Button";
 
 import './App.css';
 import logo from './img/logo.svg';
@@ -22,8 +23,12 @@ export const App = () => {
   const apiKey = '?api_key=14800cc9ec0087dda08a2762746a6750'
   const getData = (type = 'popular', arg = '') => (axios.get(apiUrl + type + apiKey + arg + '&language=' + lang))
 
-  const handleGetFilm = (type) => getData(type).then(response => setFilmData(response.data.results))
+  const handleGetFilm = (type) => getData(type).then(response => {
+    setFilmData(response.data.results)
+    setFilmDetails(null)
+  })
   const handleGetFullInfo = (id) => getData(id).then(response => setFilmDetails(response.data))
+  const handleBack = () => setFilmDetails(null)
 
   useEffect(() => {
     getData().then(response => setFilmData(response.data.results))
@@ -37,16 +42,21 @@ export const App = () => {
         <Menu handleGetFilm={handleGetFilm} setTitle={setTitle}/>
       </header>
       <main className="main">
-        <h1 className="page-title">{title}</h1>
-
-        {filmDetails && <FullInfo film={filmDetails} />}
-
-        {!isLoading ?
-            <FilmList filmData={filmData} handleGetFullInfo={handleGetFullInfo} />
+        {!filmDetails ?
+            <React.Fragment>
+              <h1 className="page-title">{title}</h1>
+              <FilmList filmData={filmData} handleGetFullInfo={handleGetFullInfo} />
+            </React.Fragment>
             :
-            <img src={loader} className="loader" alt="loader" />
+            <React.Fragment>
+              <Button className="button-back" variant="outlined" color="primary" onClick={() => handleBack()}>Back</Button>
+              <FullInfo film={filmDetails} />
+            </React.Fragment>
         }
       </main>
     </div>
   );
 }
+
+
+// <img src={loader} className="loader" alt="loader" />
